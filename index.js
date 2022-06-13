544b2151f41d39e06ed86968c518cacf06dd7d38
IgorBabikov <igorbabikov91@gmail.com>
2022-06-13 07:11:36 +0300

    added jwt

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000
+const secretKey = process.env.SECRET_KEY
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
    res.send('get main page!')
+})
+
+app.post('/auth/login', (req, res) => {
+
+   const token = jwt.sign({
+      email: req.body.email,
+      fullName: 'Игорь Бабиков'
+   }, secretKey)
+
+   res.json({
+      success: true,
+      token
+   })
 })
 
 app.listen(port, (err) => {
