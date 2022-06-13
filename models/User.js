8d0fa34d1bac7f296b0921c0af1b909639f27b11
IgorBabikov <igorbabikov91@gmail.com>
2022-06-13 22:57:15 +0300

    added model User

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+   fullName: {
+      type: String,
+      required: true
+   },
+
+   email: {
+      type: String,
+      required: true,
+      unique: true
+   },
+
+   password: {
+      type: String,
+      required: true
+   },
+
+   avatarUrl: String
+}, {
+   timestamps: true
+})
+
+
+module.exports = mongoose.model('User', UserSchema)
