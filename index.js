2b056e90a802fcf9c58ed238b009c67ac1aeea2a
IgorBabikov <igorbabikov91@gmail.com>
2022-06-13 07:23:06 +0300

    connected BD

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000
 const secretKey = process.env.SECRET_KEY
+const login = process.env.LOGIN
+const password = process.env.PASSWORD
+
+mongoose.connect(`mongodb://${login}:${password}@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/brsdtk9edfmbpfn?replicaSet=rs0`)
+.then(() => console.log('Start DB'))
+.catch(() => console.log('error'))
+
+
 
 app.use(express.json())
 
