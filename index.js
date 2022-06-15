0ae94992cb9d475dfece93e4edf4793e6865b694
Igor <igorbabikov91@gmail.com>
2022-06-15 03:00:05 +0300

    added authorization for user

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ import { registerValidation } from './validations/auth.js'
 import {validationResult} from 'express-validator'
 import UserSchema from './models/User.js'
 
+dotenv.config()
 
 const app = express()
-dotenv.config()
 const port = process.env.PORT || 3000
 const secretKey = process.env.SECRET_KEY
 const login = process.env.LOGIN
@@ -32,18 +32,31 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
       const password = req.body.password;
       const salt = await bcrypt.genSalt(10);
-      const passwordHash = await bcrypt.hash(password, salt)
+      const hash = await bcrypt.hash(password, salt)
 
       const doc = new UserSchema({
          email: req.body.email,
-         passwordHash,
+         passwordHash: hash,
          fullName: req.body.fullName,
          avatarUrl: req.body.avatarUrl
       })
 
       const user = await doc.save()
 
-      res.json(user)
+      const token = jwt.sign({
+         _id: user._id
+      }, secretKey, {
+         expiresIn: '30d'
+      })
+
+
+      const {passwordHash, ...userData} = user._doc
+
+      res.json({
+         ...userData,
+         token
+      })
+
    } catch(e) {
       console.log(e)
       res.status(500).json({
@@ -51,6 +64,47 @@ app.post('/auth/register', registerValidation, async (req, res) => {
       })
    }
 })
+
+
+app.post('/auth/login', async (req, res) => {
+   try {
+    const user = await UserSchema.findOne({email: req.body.email})
+
+    if (!user) {
+      return res.status(404).json({
+         message: 'Пользователь не найден'
+      })
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+    if (!isValidPass) {
+      return res.status(400).json({
+         message: 'Неверный логин или пароль'
+      })
+    }
+
+    const token = jwt.sign({
+      _id: user._doc
+    }, secretKey, {
+      expiresIn: '30d'
+    })
+
+    const {passwordHash, ...userData} = user._doc
+
+    res.json({
+      ...userData,
+      token
+    })
+
+   } catch(e) {
+      console.log(e)
+      res.status(500).json({
+         message: 'Не удалось авторизоваться'
+      })
+   }
+})
+
 
 
 
